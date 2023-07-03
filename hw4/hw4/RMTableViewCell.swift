5e5db39e342a7a99104749cc2cd737f2f3407309
Ян Козыренко <kopn48@mail.ru>
2023-07-03 14:50:06 +0300

    hm4

---
+++ hw4/hw4/RMTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RMTableViewCell.swift
+//  HW4
+//
+//  Created by Ян Козыренко on 01.07.2023.
+//
+
+import UIKit
+
+class RMTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ hw4/hw4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4
+//
+//  Created by Ян Козыренко on 01.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
