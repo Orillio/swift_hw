91e4ea95b237c8f627c2a0bac705db349b419245
Ян Козыренко <kopn48@mail.ru>
2023-07-10 20:32:14 +0300

    hw6

---
+++ hw4/hw4/Container.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  HW4
+//
+//  Created by Ян Козыренко on 10.07.2023.
+//
+
+import Foundation
---
+++ hw4/hw4/NetworkAvailablility.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkAvailablility.swift
+//  HW4
+//
+//  Created by Ян Козыренко on 10.07.2023.
+//
+
+import Foundation
